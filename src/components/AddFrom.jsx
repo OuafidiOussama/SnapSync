eab00aa1b19280aa3eae9541c5049e6ab2648e85
OUAFIDI <o.ouafidi@shellboxes.com>
2023-11-16 15:31:13 +0100

    Link the back end to the front and and retriving data from back end to be displayed and looped through

--- src/components/AddFrom.jsx
+++ src/components/AddFrom.jsx
@@ -1,12 +1,41 @@
+import axios from "axios"
+import { useState } from "react"
+import FileBase from "react-file-base64";
 export function AddFrom(){
+
+  const initialState = {
+    creator : "",
+    title: "",
+    message: "",
+    tags: "",
+    picture: ""
+  }
+
+  const [data, setData] = useState(initialState)
+  
+  const addPostHandler = async (e) =>{
+    e.preventDefault()
+    try {
+      console.log(data)
+      const res = await axios.post('http://localhost:5000/api/posts', data);
+      console.log(res)
+      setData(initialState)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
     return (
-        <div className=" absolute right-0 -translate-x-24  text-center bg-white shadow-lg w-80 rounded-xl py-5 px-10">
+      <form onSubmit={addPostHandler}>
+        <div className="absolute right-0 -translate-x-24  text-center bg-white shadow-lg w-80 rounded-xl py-5 px-10">
           <p className="text-md font-medium pb-2">{"Creating A Memory" || 'Editing '+'"'+''+ '"' }</p>
           <div className="gap-y-4 flex flex-col">
             <div className="relative h-11 w-full min-w-[200px]">
               <input
                 className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                 placeholder=" "
+                name="creator"
+                onChange={e => setData({ ...data, creator: e.target.value })}
               />
               <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-blue-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                 Creator
@@ -16,6 +45,8 @@ export function AddFrom(){
               <input
                 className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                 placeholder=" "
+                name="title"
+                onChange={e => setData({ ...data, title: e.target.value })}
               />
               <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-blue-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                 Title
@@ -25,6 +56,8 @@ export function AddFrom(){
               <textarea
                 className="h-full resize-none peer w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                 placeholder=" "
+                name="message"
+                onChange={e => setData({ ...data, message: e.target.value })}
               ></textarea>
               <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-blue-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                 Message
@@ -34,23 +67,26 @@ export function AddFrom(){
               <input
                 className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                 placeholder=" "
+                name="tags"
+                onChange={e => setData({ ...data, tags: e.target.value })}
               />
               <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-blue-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                 Tags
               </label>
             </div>
             <div className="relative w-full min-w-[200px]">
-                <input
-                  className="cursor-pointer relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-sm font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
-                  type="file"
-                id="formFile"
-                />
+            <FileBase
+                type="file"
+                multiple={false}
+                onDone={({ base64 }) => setData({ ...data, picture: base64 })}
+              />
             </div>
             <div className="flex flex-col gap-y-2">
-                <button className="bg-indigo-700 hover:bg-indigo-900 text-white font-bold py-2 w-full rounded transition-ease duration-300">SUBMIT</button>
-                <button className="bg-red-600 hover:bg-red-800 text-white font-bold py-2 w-full rounded transition-ease duration-300">CANCEL</button>
+                <button className="bg-indigo-700 hover:bg-indigo-900 text-white font-bold py-2 w-full rounded transition-all duration-300" type="submit">SUBMIT</button>
+                <button className="bg-red-600 hover:bg-red-800 text-white font-bold py-2 w-full rounded transition-all duration-300">CANCEL</button>
             </div>
           </div>
         </div>
+      </form>
     )
 }
--- src/components/PostContainer.jsx
+++ src/components/PostContainer.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import PostCard from './PostCard';
+import { useEffect, useState } from "react"
+import axios from 'axios';
 
 export const PostContainer = () => {
+  const [posts, setPosts] = useState('')
+  const getPosts = async ()=>{
+    await axios.get("http://localhost:5000/api/posts")
+    .then(res=>{
+      setPosts(res.data.posts)
+      // console.log(res.data.posts[0]);
+    }).catch(err=> console.error(err))
+  }
+  useEffect(()=>{
+    getPosts()
+  }, [])
   return (
-    <div className="flex flex-wrap">
-    <PostCard className="w-full" />
-    <PostCard className="w-full" />
-  </div>
+    <div className="w-9/12 flex gap-5 flex-wrap">
+      {
+        posts[0]?.map(post=> <PostCard post={post} key={post._id}/>)
+      }
+    </div>
   );  
 }
